d406f2c961f99fbf23be83cb33dad2b7a5e64188
aygulpardane <aygulparpucu@gmail.com>
2023-09-28 19:13:37 -0600

    break the navbar and main container into separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 import './App.css'
 
-import { Routes, Route, Link } from "react-router-dom";
-import Red from "./components/Red";
-import Blue from "./components/Blue";
-import Home from "./components/Home";
+import Navbar from './components/Navbar';
+import MainContainer from './components/MainContainer';
 
 function App() {
   return (
     <>
       <div id="container">
-        <div id="navbar">
-          <Link to="/blue">BLUE</Link>
-          <Link to="/red">RED</Link>
-          <Link to="/">HOME</Link>
-        </div>
-        <div id="main-section">
-          <Routes>
-            <Route path="/blue" element={<Blue />} />
-            <Route path="/red" element={<Red />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </div>
+        <Navbar />
+        <MainContainer />
       </div>
     </>
   )
